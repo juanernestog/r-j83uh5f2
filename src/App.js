f6a9d5a50ae4a09c6223e79e3676eea46b8ea3ca
JuanE <juanernestogranados@gmail.com>
2022-05-19 14:44:56 -0500

    implemented count and update count.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    this.updateCount = this.updateCount.bind(this);
+  }
+  updateCount(event) {
+    this.setState({ count: event.target.value.length });
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.updateCount}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
